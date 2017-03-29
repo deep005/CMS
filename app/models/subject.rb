206a212303c488f9956ada8d1a01b701e7759e4f
Deep Shanker <guhadeepshanker@gmail.com>
2017-03-29 20:52:23 +0530

    error helper

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -8,4 +8,6 @@ class Subject < ApplicationRecord
     scope :sorted, lambda { order("position ASC") }
     scope :newest_first, lambda { order("created_at DESC") }
     scope :search, lambda { where(["name LIKE ?" , "%{query}%"]) }
+
+    validates_presence_of :name
 end
